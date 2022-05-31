d2aea0466143976642dfa8b41c8e321ad74fa317
FantasyPvP <harry.blaise.irving@gmail.com>
2022-05-31 10:11:23 +0100

    .
    .

--- main.py
+++ main.py
@@ -15,6 +15,27 @@ client = discord.Client()
 load_dotenv()
 token = os.getenv("token")
 
+def calc(equation):
+    return 1
+
+async def exec_command(message):
+    command = message.content[2:]
+    if len(command) < 4:
+        await message.channel.send("hmm that command doesnt make sense, try '$ help'")
+        return
+    cmd_base = command[:4]
+
+    if cmd_base == "calc":
+        await message.channel.send(f"the answer to {command[5:]} is: {calc(command[5:])}")
+        return
+    if cmd_base == "help":
+        await message.channel.send(f"""
+you can chat to me by prefixing messages with '. '
+you can also run commands by typing '$ (command) [args]', for example:
+    '$ calc 1+1'
+    '$ roll user*'
+""")
+
 
 @client.event
 async def on_message(message):
@@ -24,7 +45,11 @@ async def on_message(message):
     if message.content.startswith(". "):
         response = chatbot.request(message.content[2:])
         await message.channel.send(response)
+        return
 
+    if message.content.startswith("$ "):
+        await exec_command(message)
+        return
+    return
 
 client.run(token)
-
